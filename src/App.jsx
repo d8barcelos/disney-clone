87215e8e890a38c787227b80e6baf10d6a2fd5a7
Diogo <d8barcelos@gmail.com>
2023-07-21 17:39:14 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Slider from "./components/Slider"
 
 function App() {
   
   return (
     <div className=''>
       <Header />
+      <Slider />
     </div>
   )
 }
